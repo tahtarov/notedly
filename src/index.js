fd66fc1d62c5578bb26acd8ad0f3e2a07e0cf3fe
Tagir Takhtarov <tahtarovrt@gmail.com>
2022-04-19 16:29:51 +0400

    feat: added simple graphql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,61 @@
 const express = require('express');
+const {ApolloServer, gql} = require('apollo-server-express');
+
+const port = process.env.port || 4000;
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+        notes: [Note!]!
+        note(id: ID!): Note
+    }
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
+    type Mutation {
+        newNote(content: String!): Note!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id)
+        }
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let note = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'adam'
+            }
+            notes.push(note);
+
+            return note;
+        }
+    }
+};
+
+let notes = [
+    {id: '1', content: 'note1', author: 'user1'},
+    {id: '2', content: 'note2', author: 'user2'},
+    {id: '3', content: 'note3', author: 'user3'},
+];
+
+
 const app = express();
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.start().then(() => {
+    server.applyMiddleware({app, path: '/api'});
+});
 
 app.get('/', (req, res) => res.send('Hello World'));
 
-app.listen(4000, () => console.log('Listening on port 4000'));
+app.listen(port, () => console.log(`Listening on port ${port}`));
